4654430a8b500057d3f23236c52be4157c2eebd2
m.dzhivanyan <m.dzhivanyan@brandquad.ru>
2020-10-08 00:05:14 +0400

    добавляет vuetify в зависимости

--- vue.config.js
+++ vue.config.js
@@ -1,28 +1,22 @@
-const path = require('path');
-
-module.exports = {
-  configureWebpack: {
-    resolve: {
-      alias: {
-        'assets': path.join(__dirname, 'src/assets'),
-        '@modules': path.join(__dirname, 'src/AppComponents'),
-        '@images': path.join(__dirname, 'src/assets/images'),
-        '@': path.join(__dirname, 'src/'),
-      },
-    },
+vumodule.exports = {
+  "configureWebpack": {
+    "resolve": {
+      "alias": {
+        "assets": "/home/maxim/WebstormProjects/todo_list/src/assets",
+        "@modules": "/home/maxim/WebstormProjects/todo_list/src/AppComponents",
+        "@images": "/home/maxim/WebstormProjects/todo_list/src/assets/images",
+        "@": "/home/maxim/WebstormProjects/todo_list/src/"
+      }
+    }
   },
-
-
-  css: {
-    loaderOptions: {
-      'stylus': {
-        import: '~assets/styl/_global.styl',
-      },
-    },
+  "css": {
+    "loaderOptions": {
+      "stylus": {
+        "import": "~assets/styl/_global.styl"
+      }
+    }
   },
-
-  chainWebpack: config => {
-    // удаляем prefetch плагин:
-    config.plugins.delete('prefetch');
-  },
-};
+  "transpileDependencies": [
+    "vuetify"
+  ]
+}
